a344523a16a611924c6c7a16d1b4056ca9b78cb2
csmate79 <36413556+csmate79@users.noreply.github.com>
2022-12-15 16:15:36 +0100

    3 files modified

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,22 +1,45 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AdType } from './enums/ad.enum';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css'],
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
   public registrationForm: FormGroup;
 
+  public AdTypeEnum = Object.keys(AdType);
+
   constructor(private fb: FormBuilder) {}
   ngOnInit() {
+    console.log(this.AdTypeEnum);
     this.registrationForm = this.fb.group({
-      name: [null],
+      name: [null, Validators.maxLength(75)],
       email: [null],
-      password: [null],
-      rePassword: [null],
-      userType: [null]
+      password: [
+        null,
+        [
+          Validators.required,
+          Validators.pattern(
+            '(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&].{8,}'
+          ),
+        ],
+      ],
+      rePassword: [
+        null,
+        [
+          Validators.required,
+          Validators.pattern(
+            '(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&].{8,}'
+          ),
+        ],
+      ],
+      userType: [null],
+      ads: [null],
+      birthDate: [null],
+      rules: [null],
     });
   }
 }
